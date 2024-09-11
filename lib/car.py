44191a73c6edc431ff1ccfbc50c310c13d5e5f00
ianramy <imwagore@gmail.com>
2024-09-12 01:04:34 +0300

    modified

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,10 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+    def __init__(self, wheel_size, wheel_number):
+        # Initialize the Car by calling the Vehicle constructor
+        super().__init__(wheel_size, wheel_number)
+
+    def go(self):
+        # Car overrides the go() method with a different sound
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,13 @@
 class Vehicle:
-    pass
+    def __init__(self, wheel_size, wheel_number):
+        # Initialize the wheel_size and wheel_number
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+    def go(self):
+        # Vehicle's method to simulate movement
+        return "vrrrrrrrooom!"
+
+    def fill_up_tank(self):
+        # Method to simulate filling the tank
+        return "filling up!"
